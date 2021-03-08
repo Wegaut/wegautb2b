8f03ef9a0cb7b1eae9c82e8ce87d7a2c1300bc83
Wegaut <jonas@wegaut.com>
2021-03-08 18:28:46 -0500

    first commit// 08/03/21 // Frontend

---
+++ routes/user.js
@@ -0,0 +1,42 @@
+'use strict'
+
+var express = require('express');
+var UserController = require('../controllers/user');
+
+
+var router = express.Router();
+var md_auth=require('../middleware/authenticated');
+
+var multpart =require('connect-multiparty');
+var md_upload=multpart({uploadDir:'./uploads/users'});
+
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination:function(req, file, cb){
+        cb(null, './uploads/users/');
+        
+    },
+
+    filename:function(req,file,cb){
+        cb(null,"user" + Date.now() + file.originalname);
+    }
+});
+ 
+const upload = multer({storage:storage});
+
+
+
+  
+router.get('/home1', UserController.home);
+router.post('/test1', UserController.test);
+router.get('/GET_USERS', UserController.getUsuarios);
+router.get('/GET_USER/:userId', UserController.getUsuario);
+router.post('/POST_USER', UserController.save);
+router.post('/POST_LOGIN', UserController.login);
+router.put('/PUT_USER/', md_auth.authenticated, UserController.update);
+router.delete('/DELETE_USER/:userId', md_auth.authenticated, UserController.deleteUsusario );
+router.post('/uploadPhotoProfile', [md_upload,md_auth.authenticated], UserController.uploadPhotoProfile);
+router.post('/uploadPhotoProfile2', [md_auth.authenticated,upload.single('photoProfile')], UserController.uploadPhotoProfile2);
+router.get('/photoProfile/:fileName', UserController.photoProfile);
+
+module.exports = router;
