69bd6ac551d5a78b8cd1fc427a5f74331dd9121a
Wegaut <jonas@wegaut.com>
2021-03-08 20:19:39 -0500

    Wegaut // 08/03/21 // Upload file

--- app.js
+++ app.js
@@ -5,23 +5,16 @@ const bodyParser = require('body-parser');
 const app = express();
 const morgan = require('morgan');
 
-
 // cargar archivos rutas
-
-
 var user_routes = require('./routes/user');
 var group_routes= require('./routes/group');
 var message_routes= require('./routes/message');
 var contact_routes = require('./routes/contact');
 
-
-
-
 // middlewares
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
-
 
 // Configurar cabeceras y cors
 app.use((req, res, next) => {
@@ -31,9 +24,7 @@ app.use((req, res, next) => {
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
 });
-
 // rutas
-
 app.get('/test',(req, res) => {
     res.status(200).send({
         message:"Hola Prueba Backend B2B bot"
@@ -46,20 +37,13 @@ app.post('/test1',(req,res) => {
         message:"Hola soy prueba"
         });
     });
-
     app.get('/chat', (req, res) => res.send('hello!'));
 
-
 // rutas
-
-
 app.use('/api', user_routes);
 app.use('/api', group_routes);
 app.use('/api', message_routes);
 app.use('/api', contact_routes);
-
-
-
 
 // exportar
 module.exports = app;
