14ce1e0edd4dcf96c6ab692aa900b8b3d5a69a40
Wegaut <jonas@wegaut.com>
2021-03-08 19:47:56 -0500

    Wegaut // 08/03/21 // Upload file

--- index.js
+++ index.js
@@ -5,21 +5,18 @@ var app = require('./app');
 const PORT = process.env.PORT || 5000;
 
 mongoose.Promise = global.Promise;
+				//mongodb://localhost:27017/Chatbot
+				//mongodb+srv://JonasAndres:Miajade212.@b2bchat.zwvdu.mongodb.net/B2BChat
 				
-/** Conexion local */										//mongodb://localhost:27017/Chatbot
-/** Conexion con el correo de prueba */						//mongodb+srv://JonasAndres:Miajade212.@b2bchat.zwvdu.mongodb.net/B2BChat
-/** Conexion con el correo de desarrollo */					//mongodb+srv://JonasAndres:Developer04@b2bdatabase.hgzdh.mongodb.net/b2bdatabase?retryWrites=true&w=majority
-
-mongoose.connect('mongodb+srv://JonasAndres:Developer04@b2bdatabase.hgzdh.mongodb.net/b2bdatabase?retryWrites=true&w=majority',{useFindAndModify: false, useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+				mongoose.connect('mongodb+srv://JonasAndres:Developer04@b2bdatabase.hgzdh.mongodb.net/b2bdatabase?retryWrites=true&w=majority',{useFindAndModify: false, useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
 .then(() => {
 	console.log("Conexión a la base de datos establecida satisfactoriamente...");
 	// Creacion del servidor
-	app.listen(PORT,  () => {
-		console.log(`Servidor corriendo correctamente en la url: localhost:${PORT}`);
+	app.listen(PORT, () => {
+		console.log(`Servidor corriendo correctamente en la url: localhost:${ PORT }`);
 	});
 })
-.catch(err => console.log(err)); 
-
+.catch(err => console.log(err));
 
 
 
